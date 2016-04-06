463329414ebed8de2fca9779cf3d110be4fcbbb8
aq5301 <ashleyq5301@att.net>
2016-04-06 16:29:03 -0500

    Update hw1.s

--- hw1.s
+++ hw1.s
@@ -10,7 +10,7 @@ main:
      MOV R2, R0
      BL scanint
      MOV R1, R0
-     @BL function 
+     BL function 
      MOV R1, R0
      BL print
      B exit
@@ -56,7 +56,6 @@ function:
     CMP R3, R0  
     MOVGE R0, R3
     MOV PC, R5
-
     BEQ product
     CMP R2, #'M'
     BEQ max
