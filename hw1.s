6fc660d291c58d1fa9d96c0fc4d1e23e1a239b8c
aq5301 <ashleyq5301@att.net>
2016-04-06 21:49:29 -0500

    Update hw1.s

--- hw1.s
+++ hw1.s
@@ -6,14 +6,12 @@ main:
      BL prompt
      BL scanint
      MOV R8, R0
-          MOV R1, R8
-    BL print
      BL scanchar
      MOV R6, R0
-
-    BL print
      BL scanint
      MOV R1, R0
+
+    BL print
      MOV R2, R8
      BL function 
      MOV R1, R0
