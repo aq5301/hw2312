8bf6d7e27f31b40d1f8ade90a7e8d9ccf8240608
aq5301 <ashleyq5301@att.net>
2016-04-22 19:25:25 -0500

    Update honors.s

--- honors.s
+++ honors.s
@@ -15,8 +15,9 @@ main:
      VLDR S1, [R0] 
      
     @ BL function 
+     VADD.F32 S2, S0, S1
      
-     VCVT.F64.F32 D4, S1 
+     VCVT.F64.F32 D4, S2 
      VMOV R1, R2, D4
      BL print
      B exit
