f1366b790f1914b4380ae666995bae1ec6924a6e
aq5301 <ashleyq5301@att.net>
2016-04-24 18:24:58 -0500

    Update hw3.s

--- hw3.s
+++ hw3.s
@@ -1,14 +1,53 @@
     .global main
     .func main
    
-main:
-    LDR R0,=print_str   @ store string address in R0
-    BL printf           @ call printf
-    B   _exit           @ branch to exit procedure with no return
    
-_exit:   
-	MOV R7, #1          @ terminate syscall, 1
-	SWI 0               @ execute syscall
+   
+main:
+    BL prompt
+    BL scanint
+    MOV R5, R0	
+    
+    
+    
+    BL print
+    B   exit           
+   
+exit:   
+    MOV R7, #1         
+    SWI 0               
+
+
+prompt:
+    MOV R7, #4
+    MOV R0, #1
+    MOV R2, #17
+    LDR R1, =prompt_str
+    SWI 0
+    MOV PC, LR
+    
+    
+print:
+
+
+generate:
+
+
+sort_ascending:
+
+
+
+scanint:
+    MOV R4, LR              
+    SUB SP, SP, #4          
+    LDR R0, =num_str     
+    MOV R1, SP              
+    BL scanf                
+    LDR R0, [SP]            
+    ADD SP, SP, #4          
+    MOV PC, R4  
+
+
 
 .data
-print_str:  .asciz "Hello World!\n"
+prompt_str:     .ascii      "Enter an integer.\n"
