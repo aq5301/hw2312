2a80d1e208d2402932bf90ab2b918eb662c4f9fa
aq5301 <ashleyq5301@att.net>
2016-04-25 21:26:56 -0500

    Update hw3.s

--- hw3.s
+++ hw3.s
@@ -23,21 +23,35 @@ prompt:
     LDR R1, =prompt_str
     SWI 0
     MOV PC, LR
-    
-    
+
+read_arrays:
+
+
+
 print:
+    PUSH {LR}
+    LDR R0, =print_str
+    BL printf
+    POP {PC}
 
 
 generate:
     CMP R0, #20
     BEQ sort_ascending
     LDR R1, =a
+    LSL R2, R0, #2
+    ADD R2, R1, R2
     
+    ADD R3, R5, R0
+    STR R3, [R2]
     
-
-
-
-    B sort_ascending
+    ADD R3, R5, R0
+    ADD R4, R3, #1
+    MUL R3, R4, -1
+    STR R3, [R2, +#1]
+    
+    ADD R0, R0, #2
+    B generate
 
 
 sort_ascending:
@@ -60,7 +74,7 @@ scanint:
 
 
 .balign 4
-a:              .skip       400
-b:              .skip       800
-printf_str:     .asciz      "a[%d] = %d\n"
+a:              .skip       80
+b:              .skip       80
+print_str:      .asciz      "array_a[%d] = %d, array_b = %d\n"
 prompt_str:     .ascii      "Enter an integer.\n"
